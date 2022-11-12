438e69602ef88152a9b5d7e358dcbaff9d89ce55
maahmmooud <mahmooud.mustafa.mohamed@gmail.com>
2022-11-12 11:52:36 +0200

    CRUD employee

--- routes/employee.js
+++ routes/employee.js
@@ -21,6 +21,21 @@ router.route('/')    //use "http://localhost:3000/employees"
 
 
 
+router.route('/:id')     //use "http://localhost:3000/employees/1"
+.put((req,res)=>{
+    const body= req.body
+    const id =req.params.id
+    db.Employee.update( body , {where: {id: id}})
+    .then(employee=>{res.json(employee)})
+    .catch((err)=> res.json(err.message))
+    })   
 
+
+
+.delete((req,res)=>{
+        db.Employee.destroy({where: {id: req.params.id} })
+        .then(employee=>{res.json(employee)})
+        .catch((err)=> res.json(err.message))
+    })
 return router
 }
